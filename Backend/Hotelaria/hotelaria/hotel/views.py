6d92d5d9323f4703b9f2dfc5dd657ad7a6295dee
Helois <heloisasenaim16@gmail.com>
2025-05-07 15:24:27 -0300

    views e models

--- Backend/Hotelaria/hotelaria/hotel/views.py
+++ Backend/Hotelaria/hotelaria/hotel/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from.models import *
 
 def Homepage(request):
-    pass 
+   context = {}
+   dados_home = homepage.objects.all()
+   context['dados_home'] = dados_home
+   return render(request, 'homepage.html')
