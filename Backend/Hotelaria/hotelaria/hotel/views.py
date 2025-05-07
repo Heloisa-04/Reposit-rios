7c821123c60033e666eaec986c0d1203d20ac87e
Helois <heloisasenaim16@gmail.com>
2025-05-07 15:57:30 -0300

    tentativas fracassadas porém uma com sucesso

--- Backend/Hotelaria/hotelaria/hotel/views.py
+++ Backend/Hotelaria/hotelaria/hotel/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from.models import *
 
-def Homepage(request):
+def home(request):
+    return render(request, 'home.html')
+
+
+def homepage(request):
    context = {}
    dados_home = homepage.objects.all()
    context['dados_home'] = dados_home
-   return render(request, 'homepage.html')
+   return render(request, 'homepage.html', context)
