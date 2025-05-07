ca7720180f5120c2b9e60aac90b3e02f913990e1
Helois <heloisasenaim16@gmail.com>
2025-05-07 10:48:05 -0300

    selecionar links

--- Front end/dom-exercicios/scripts/selecionar/link-interno.js
+++ Front end/dom-exercicios/scripts/selecionar/link-interno.js
@@ -1,3 +1,3 @@
-// 
+// Proucura os links internos e espefica que tem que ser os links com "#"
 const linksInternos = document.querySelectorAll('a[href^="#"]');
 console.log("Links internos:", linksInternos);
