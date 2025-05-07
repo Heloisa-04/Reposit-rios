9f27aeaf669893d7c677874d76b600ce79efc85d
Helois <heloisasenaim16@gmail.com>
2025-05-07 10:57:35 -0300

    introdução e link interno

---
+++ Front end/dom-exercicios/scripts/selecionar/h2-introduc.js
@@ -0,0 +1,3 @@
+// variavel --função que proucura os elementos-- passamos o elemento html que possui esta classe css.
+
+console.log("Primeiro h2 em .introducao:", h2_intro);
--- Front end/dom-exercicios/scripts/selecionar/link-interno.js
+++ Front end/dom-exercicios/scripts/selecionar/link-interno.js
@@ -1,3 +1,5 @@
 // Proucura os links internos e espefica que tem que ser os links com "#"
 const linksInternos = document.querySelectorAll('a[href^="#"]');
+
+//imprime no console
 console.log("Links internos:", linksInternos);
