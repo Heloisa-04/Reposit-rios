509407c6ddaa9a7aa5e5087b5d5ae8d0118823fd
Helois <heloisasenaim16@gmail.com>
2025-05-09 08:41:29 -0300

    tela password feita

--- Mobile/componentizacao-flutter-main/componentizacao-flutter-main/lib/ui/screens/password_screen.dart
+++ Mobile/componentizacao-flutter-main/componentizacao-flutter-main/lib/ui/screens/password_screen.dart
@@ -1,18 +1,34 @@
 import 'package:componentes_flutter/ui/widgets/custom_textfield.dart';
+import 'package:componentes_flutter/ui/widgets/custom_button.dart';
+import 'package:componentes_flutter/ui/widgets/custom_textbutton.dart';
+import 'package:componentes_flutter/ui/widgets/custom_title.dart';
 import 'package:flutter/material.dart';
+import 'package:componentes_flutter/ui/screens/create_account.dart';
 
 class PasswordScreen extends StatelessWidget {
   const PasswordScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
           child: Column(
             children: [
-              CustomTextfield(hintText: "Password", obscure: true,)
+              CustomTitle(text: "Sign-in",),
+              SizedBox(height: 24,),
+              CustomTextfield(hintText: "Password", obscure: true,),
+              SizedBox(height: 14,),
+              CustomButton( text: "Continue",  onPressed:(){},),
+              CustomTextbutton(text: "Dont have Account", textButton: "Create", onPressed: () {
+                    Navigator.of(context).push(
+                            MaterialPageRoute(
+                                builder: (context) => const CreateAccount(),
+                              ),
+                    );
+              },
+              ),
             ],
           ),
         ),
