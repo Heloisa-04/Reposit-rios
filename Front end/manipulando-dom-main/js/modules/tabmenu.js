2c9f75417390b913b11e57ba842b00a6ac2e98dd
Helois <heloisasenaim16@gmail.com>
2025-05-14 09:11:13 -0300

    modal.js

--- Front end/manipulando-dom-main/js/modules/tabmenu.js
+++ Front end/manipulando-dom-main/js/modules/tabmenu.js
@@ -1,8 +1,4 @@
 export default function tabMenuInit(){
-    document.querySelectorAll('[data-tab="menu"] li')
-    document.querySelectorAll('[]')
-
-
     // faça a lógica sempre utilizando uma função para exportação
     // essa função sera inicializada no index.js
 }
