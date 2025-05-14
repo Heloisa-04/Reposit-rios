5b9a3ff55527cd2605b74ea866b943733d399543
Helois <heloisasenaim16@gmail.com>
2025-05-14 08:28:54 -0300

    mano

--- Front end/manipulando-dom-main/manipulando-dom-main/js/index.js
+++ Front end/manipulando-dom-main/manipulando-dom-main/js/index.js
@@ -1,19 +1,20 @@
-import accordionInit from "./modules/accordion.js";
-import tabMenuInit from "./modules/tabmenu.js";
+// import accordionInit from "./modules/accordion.js";
+// import tabMenuInit from "./modules/tabmenu.js";
 import menuMobileInit from "./modules/menuMobile.js";
-import { Modal } from "./modules/modal.js";
+// import { Modal } from "./modules/modal.js";
 
-tabMenuInit()
-accordionInit()
+// tabMenuInit()
+// accordionInit()
 menuMobileInit()
 
-new Modal('.js-modal',  '.modal-close', '.modal-forms', '.js-modal-blur').init();
+// new Modal('.js-modal',  '.modal-close', '.modal-forms', '.js-modal-blur').init();
 
 
 
 
 const imagens = document.querySelectorAll(".natureza-img");
 const conteudos = document.querySelectorAll(".natureza-content");
+
 
 imagens.forEach((img, index) => {
   img.addEventListener("click", () => {
