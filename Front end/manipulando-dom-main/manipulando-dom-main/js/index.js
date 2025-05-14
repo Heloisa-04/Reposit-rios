f0ab018d87207dac75bc7fa0a0494c117d6420cc
Helois <heloisasenaim16@gmail.com>
2025-05-14 08:22:18 -0300

    tabmenu

--- Front end/manipulando-dom-main/manipulando-dom-main/js/index.js
+++ Front end/manipulando-dom-main/manipulando-dom-main/js/index.js
@@ -1,16 +1,26 @@
-// import accordionInit from "./modules/accordion.js";
-// import tabMenuInit from "./modules/tabmenu.js";
+import accordionInit from "./modules/accordion.js";
+import tabMenuInit from "./modules/tabmenu.js";
 import menuMobileInit from "./modules/menuMobile.js";
-// import { Modal } from "./modules/modal.js";
+import { Modal } from "./modules/modal.js";
 
-// tabMenuInit()
-// accordionInit()
+tabMenuInit()
+accordionInit()
 menuMobileInit()
 
-// new Modal('.js-modal', 
-// '.modal-close', 
-// '.modal-forms', '.js-modal-blur').init();
+new Modal('.js-modal',  '.modal-close', '.modal-forms', '.js-modal-blur').init();
 
 
 
 
+const imagens = document.querySelectorAll(".natureza-img");
+const conteudos = document.querySelectorAll(".natureza-content");
+
+imagens.forEach((img, index) => {
+  img.addEventListener("click", () => {
+    // Oculta todos os conteúdos
+    conteudos.forEach(c => c.classList.remove("active"));
+
+    // Mostra apenas o conteúdo correspondente à imagem clicada
+    conteudos[index].classList.add("active");
+  });
+});
--- Front end/manipulando-dom-main/manipulando-dom-main/js/modules/tabmenu.js
+++ Front end/manipulando-dom-main/manipulando-dom-main/js/modules/tabmenu.js
@@ -1,4 +1,8 @@
 export default function tabMenuInit(){
+    document.querySelectorAll('[data-tab="menu"] li')
+    document.querySelectorAll('[]')
+
+
     // faça a lógica sempre utilizando uma função para exportação
     // essa função sera inicializada no index.js
 }
