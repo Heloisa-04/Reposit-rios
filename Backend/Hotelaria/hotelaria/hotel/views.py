8710207d9053dcd1b84a3ec3a5eacc8e75cddf6e
Helois <heloisasenaim16@gmail.com>
2025-05-14 14:21:30 -0300

    Instalação do django no ambiente virtual

--- Backend/Hotelaria/hotelaria/hotel/views.py
+++ Backend/Hotelaria/hotelaria/hotel/views.py
@@ -5,8 +5,3 @@ def home(request):
     return render(request, 'home.html')
 
 
-def homepage(request):
-   context = {}
-   dados_home = homepage.objects.all()
-   context['dados_home'] = dados_home
-   return render(request, 'homepage.html', context)
